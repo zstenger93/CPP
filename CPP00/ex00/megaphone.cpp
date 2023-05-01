fda42464bcc150d79d16c6dae9e9f842114c2c8f
zstenger <zstenger@student.42heilbronn.de>
2023-05-01 07:29:31 +0200

    00 ex 00

--- CPP00/ex00/megaphone.cpp
+++ CPP00/ex00/megaphone.cpp
@@ -6,10 +6,7 @@ int	main(int argc, char **argv)
 	std::string		s;
 
 	if (argc == 1)
-	{
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
-		return (0);
-	}
+		return (std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl, 0);
 	for (int i = 1; i < argc; i++)
 	{
 		s = argv[i];
@@ -18,6 +15,5 @@ int	main(int argc, char **argv)
 			s[k] = std::toupper(s.at(k));
 		std::cout << s;
 	}
-	std::cout << std::endl;
-	return (0);
+	return (std::cout << std::endl, 0);
 }
--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 <h1 align="center">
 	📖 C++
 </h1>
+# CPP00
+namespace:
+
+gruping related declarations together under a single name
