8a9e584c4169841e202a12b52988fd1c9fffb7df
Zsolt Stenger <115490237+zstenger93@users.noreply.github.com>
2023-05-27 09:29:42 +0200

    01 fininsh

--- CPP02/ex00/src/Fixed.cpp
+++ CPP02/ex00/src/Fixed.cpp
@@ -3,19 +3,22 @@
 Fixed::Fixed() { std::cout << CONSTRUCTOR << std::endl; }
 Fixed::~Fixed() { std::cout << DESTRUCTOR << std::endl; }
 
+/*
+to make a copy of an object, creating a new object with the same values as the original.
+*/
 Fixed::Fixed(Fixed const &number) {
-    std::cout << COPYCONS << std::endl;
-    *this = number;
+	std::cout << COPYCONS << std::endl;
+	*this = number;
 }
 
 int Fixed::getRawBits(void) const { return std::cout << GETRAWBITS << std::endl, _fixed; }
 void Fixed::setRawBits(int const raw) {
-    std::cout << SETRAWBITS << std::endl;
-    _fixed = raw;
+	std::cout << SETRAWBITS << std::endl;
+	_fixed = raw;
 }
 
 Fixed &Fixed::operator=(Fixed const &number) {
-    std::cout << COPYASSIGN << std::endl;
-    if (this != &number) this->_fixed = number.getRawBits();
-    return *this;
+	std::cout << COPYASSIGN << std::endl;
+	if (this != &number) this->_fixed = number.getRawBits();
+	return *this;
 }
