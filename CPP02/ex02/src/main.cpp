e273235d7dc6457086335af7383d129b4c9a407f
Zsolt Stenger <115490237+zstenger93@users.noreply.github.com>
2023-06-02 08:09:49 +0200

    more test

--- CPP02/ex02/src/main.cpp
+++ CPP02/ex02/src/main.cpp
@@ -11,6 +11,7 @@ int main(void) {
 	std::cout << a << std::endl;
 	std::cout << b << std::endl;
 	std::cout << Fixed::max(a, b) << std::endl;
+	std::cout << Fixed::min(a, b) << std::endl;
 
 	return 0;
 }
