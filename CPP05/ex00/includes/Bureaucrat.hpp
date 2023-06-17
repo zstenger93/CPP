64f4c50000b9482e21e0b409077ddbab1ef73594
Zsolt Stenger <115490237+zstenger93@users.noreply.github.com>
2023-06-17 14:15:04 +0200

    05 init

--- CPP05/ex00/includes/Bureaucrat.hpp
+++ CPP05/ex00/includes/Bureaucrat.hpp
@@ -1,26 +1,48 @@
 #ifndef BUREAUCRAT_HPP
 #define BUREAUCRAT_HPP
 
+#include <exception>
 #include <iostream>
+#include <ostream>
 
 #define BLUE "\033[1;34m"
 #define YELLOW "\033[1;33m"
 #define RED "\033[1;31m"
 #define END "\033[1;39m"
 
+#define CONSTRUCTOR "Bureaucraft default constructor called."
+#define GTL "Provided grade is too low."
+#define GTH "Provided grade is too high."
+
 class Bureaucrat {
    public:
 	Bureaucrat();
-	Bureaucrat(std::string const &name);
+	Bureaucrat(std::string const &name, int grade);
 	Bureaucrat(Bureaucrat const &cpy);
 	~Bureaucrat();
-	Bureaucrat &operator<<(Bureaucrat const &rhs);
+	Bureaucrat &operator=(Bureaucrat const &rhs);
+	static const int MinGrade = 1;
+	static const int MaxGrade = 150;
 	std::string const &getName() const;
-	void getGrade();  // lowest 1 highest is 150
+	void getGrade() const;	// lowest 1 highest is 150
+	void IncrementGrade();
+	void DecrementGrade();
 
-   protected:
+   private:
 	int grade;
 	std::string const name;
+
+	class GradeTooLowExecption : public std::exception {
+	   public:
+		virtual const char *what() const throw();
+	};
+
+	class GradeTooHighExecption : public std::exception {
+	   public:
+		virtual const char *what() const throw();
+	};
 };
+
+std::ostream &operator<<(std::ostream &os, Bureaucrat const &bureaucrat);
 
 #endif
---
+++ CPP05/ex00/src/Bureaucrat.cpp
@@ -0,0 +1,17 @@
+#include "../includes/Bureaucrat.hpp"
+
+Bureaucrat::Bureaucrat() {
+
+}
+
+Bureaucrat::Bureaucrat(std::string const &name, int grade) : name(name), grade(grade) {
+
+}
+
+Bureaucrat::Bureaucrat(Bureaucrat const &cpy) {
+	
+}
+
+Bureaucrat::~Bureaucrat() {
+	
+}
