6447040e1d0c68783adadfc34ef394a279b39ad7
Zsolt Stenger <zstenger@student.42heilbronn.de>
2023-07-23 07:45:08 +0200

    updated main test

--- CPP06/ex01/src/main.cpp
+++ CPP06/ex01/src/main.cpp
@@ -9,9 +9,14 @@ int main() {
 	dataToSerialize->str = "nani?!";
 	dataToSerialize->c = 'c';
 	dataToSerialize->i = 4;
+	std::cout << "Data to serialize:" << std::endl;
+	std::cout << dataToSerialize->str << std::endl;
+	std::cout << dataToSerialize->c << std::endl;
+	std::cout << dataToSerialize->i << std::endl;
 	raw = takeDataDo.serialize(dataToSerialize);
 	std::cout << "raw value: " << raw << std::endl;
 	deserializedData = takeDataDo.deserialize(raw);
+	std::cout << "Deserialized data:" << std::endl;
 	std::cout << deserializedData->str << std::endl;
 	std::cout << deserializedData->c << std::endl;
 	std::cout << deserializedData->i << std::endl;
