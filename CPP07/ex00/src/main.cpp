9638dff5ac62903835f1d6d3b37f9e07cd1a5566
Zsolt Stenger <zstenger@student.42heilbronn.de>
2023-08-02 13:51:45 +0200

    more readable printing

--- CPP07/ex00/src/main.cpp
+++ CPP07/ex00/src/main.cpp
@@ -3,15 +3,22 @@
 int main(void) {
 	int a = 2;
 	int b = 3;
+	std::cout << "\033[1;4;31mBefore swap:\033[0;0;39m "
+			  << "a = " << a << ", b = " << b << std::endl;
 	::swap(a, b);
-	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "Swappin..." << std::endl;
+	std::cout << "\033[1;4;31mAfter swap:\033[0;0;39m "
+			  << "a = " << a << ", b = " << b << std::endl;
 	std::cout << "min( a, b ) = " << ::min(a, b) << std::endl;
 	std::cout << "max( a, b ) = " << ::max(a, b) << std::endl;
 	std::string c = "chaine1";
 	std::string d = "chaine2";
-	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "\033[1;4;31mBefore swap:\033[0;0;39m "
+			  << "c = " << c << ", d = " << d << std::endl;
 	::swap(c, d);
-	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "Swappin..." << std::endl;
+	std::cout << "\033[1;4;31mAfter swap:\033[0;0;39m "
+			  << "c = " << c << ", d = " << d << std::endl;
 	std::cout << "min( c, d ) = " << ::min(c, d) << std::endl;
 	std::cout << "max( c, d ) = " << ::max(c, d) << std::endl;
 	return 0;
--- CPP07/ex01/src/main.cpp
+++ CPP07/ex01/src/main.cpp
@@ -1,24 +1,58 @@
 #include "../includes/iter.hpp"
 
+class Awesome {
+   public:
+	Awesome(void) : _n(42) { return; }
+	int get(void) const { return this->_n; }
+
+   private:
+	int _n;
+};
+
+std::ostream& operator<<(std::ostream& o, Awesome const& rhs) {
+	o << rhs.get();
+	return o;
+}
+
+template <typename T>
+void print(T& x) {
+	std::cout << x << std::endl;
+	return;
+}
+
+void fromPDF() {
+	int tab[] = {0, 1, 2, 3, 4};
+	Awesome tab2[5];
+
+	iter(tab, 5, print<const int>);
+	iter(tab2, 5, print<Awesome>);
+}
+
 int main() {
 	int intArray[] = {1, 2, 3, 4, 5};
-  size_t intArrayLength = sizeof(intArray) / sizeof(int);
+	size_t intArrayLength = sizeof(intArray) / sizeof(int);
 
-  double doubleArray[] = {1.1, 2.2, 3.3, 4.4, 5.5};
-  size_t doubleArrayLength = sizeof(doubleArray) / sizeof(double);
+	double doubleArray[] = {1.1, 2.2, 3.3, 4.4, 5.5};
+	size_t doubleArrayLength = sizeof(doubleArray) / sizeof(double);
 
-  char charArray[] = {'a', 'b', 'c', 'd', 'e'};
-  size_t charArrayLength = sizeof(charArray) / sizeof(char);
+	char charArray[] = {'a', 'b', 'c', 'd', 'e'};
+	size_t charArrayLength = sizeof(charArray) / sizeof(char);
 
-  // Using iter with intArray
-  iter(intArray, intArrayLength, printNum<int>);
-  std::cout << std::endl;
+	// Using iter with intArray
+	std::cout << "\033[1;4;31mIterating through an INT array:\033[0;0;39m" << std::endl;
+	iter(intArray, intArrayLength, printNum<int>);
+	std::cout << std::endl;
 
-  // Using iter with doubleArray
-  iter(doubleArray, doubleArrayLength, printNum<double>);
-  std::cout << std::endl;
+	// Using iter with doubleArray
+	std::cout << "\033[1;4;31mIterating through a DOUBLE array:\033[0;0;39m" << std::endl;
+	iter(doubleArray, doubleArrayLength, printNum<double>);
+	std::cout << std::endl;
 
-  // Using iter with charArray
-  iter(charArray, charArrayLength, printNum<char>);
-  std::cout << std::endl;
+	// Using iter with charArray
+	std::cout << "\033[1;4;31mIterating through a CHAR array:\033[0;0;39m" << std::endl;
+	iter(charArray, charArrayLength, printNum<char>);
+	std::cout << std::endl;
+
+	std::cout << "\033[1;4;31mTest from PDF:\033[0;0;39m" << std::endl;
+	fromPDF();
 }
