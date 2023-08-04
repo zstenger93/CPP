a831b25207ae6c52f5c96612f65753d579c611f9
Zsolt Stenger <zstenger@student.42heilbronn.de>
2023-08-04 02:22:56 +0200

    07 extra test

--- CPP07/ex00/src/main.cpp
+++ CPP07/ex00/src/main.cpp
@@ -1,5 +1,29 @@
 #include "../includes/whatever.hpp"
 
+class Awesome {
+   public:
+	Awesome(void) : _n(0) {}
+	Awesome(int n) : _n(n) {}
+	Awesome &operator=(Awesome &a) {
+		_n = a._n;
+		return *this;
+	}
+	bool operator==(Awesome const &rhs) const { return (this->_n == rhs._n); }
+	bool operator!=(Awesome const &rhs) const { return (this->_n != rhs._n); }
+	bool operator>(Awesome const &rhs) const { return (this->_n > rhs._n); }
+	bool operator<(Awesome const &rhs) const { return (this->_n < rhs._n); }
+	bool operator>=(Awesome const &rhs) const { return (this->_n >= rhs._n); }
+	bool operator<=(Awesome const &rhs) const { return (this->_n <= rhs._n); }
+	int get_n() const { return _n; }
+
+   private:
+	int _n;
+};
+std::ostream &operator<<(std::ostream &o, const Awesome &a) {
+	o << a.get_n();
+	return o;
+}
+
 int main(void) {
 	int a = 2;
 	int b = 3;
@@ -21,5 +45,13 @@ int main(void) {
 			  << "c = " << c << ", d = " << d << std::endl;
 	std::cout << "min( c, d ) = " << ::min(c, d) << std::endl;
 	std::cout << "max( c, d ) = " << ::max(c, d) << std::endl;
+
+
+	Awesome x(2), l(4);
+
+	swap(x, l);
+	std::cout << x << " " << l << std::endl;
+	std::cout << max(x, l) << std::endl;
+	std::cout << min(x, l) << std::endl;
 	return 0;
 }
